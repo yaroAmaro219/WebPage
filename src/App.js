80da421ce54e059d9942b20ab3acdd6f59c5f048
Artemie Amari <yaroamari@gmail.com>
2020-08-10 00:37:50 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ const App = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg>
           </a>
-          <p>Developed a one page funnel website for a Tennis camp</p>
+          <p>Developed a full-stack web-application using surge and heroku to host it on the internet. The front-end portion was built using React.js with react-router to create different pages as well desig using css. Used Ruby on Rails on the back end to create a database to store data from enrollment form. Configured DNS to netlify to host with a secure connection on the internet. Applied google analytics and running online promotion campaign. </p>
           <p class="highlights"> React.js</p>
             </div>
             <div class="work-piece">
@@ -215,10 +215,12 @@ const App = () => {
  
   
       <div class='contact' id='contact'>
-        <h1>Contact</h1>
+        <div>
+          <h1 class='contact-title'>Contact</h1>
+          </div>
       <div class="email">
           <a href="mailto:yaroamari@gmail.com"><svg class="large-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 21h-24v-18h24v18zm-23-16.477v15.477h22v-15.477l-10.999 10-11.001-10zm21.089-.523h-20.176l10.088 9.171 10.088-9.171z"></path></svg></a>
-          <a>yaroamari@gmail.com</a>
+          <a href='mailto:yaroamari@gmail.com'>yaroamari@gmail.com</a>
           </div>
       </div>
      
