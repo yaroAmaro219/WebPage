5dbafbe1e1a34744fb187fdd4e48718b9b90a055
Artemie Amari <yaroamari@gmail.com>
2020-08-11 07:15:11 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Nav from "./components/Nav";
 import Ball from './Tennis-Ball-Download-Free-PNG.png'
+import Pups from './images/PowerPups.jpg'
 import Coca from './components/Favicon_Icon.svg'
 import Tennis from './Screen Shot 2020-07-09 at 4.30.18 PM.png'
 import Youtube from './Screen Shot 2020-07-16 at 8.25.05 AM.png'
@@ -24,18 +25,18 @@ const App = () => {
         {/* <Link to="/">
           <img class="image" src={Image} />
         </Link> */}
-        <div class="center-header">
+     
           <Link to="/">
             <h2 class="welcome">Artemie Amari</h2>
           </Link>
-        </div>
+  
         <div class='nav-container'>
         <Nav />
         </div>
       </div>
 
       <div class='about' id='about'>
-        <h1>Hi, I'm Artemie Amari.</h1>
+        <h1 class='intro'>Hi, I'm Artemie Amari.</h1>
         <div class="about-main">
         {/* <img class="image" src={Image} /> */}
           <div class="text">
@@ -54,7 +55,7 @@ const App = () => {
                 </a>
               </div>
               <div class="seen">
-              <a href="https://www.youtube.com/watch?v=xtbEpU1VjFE" target="_blank" rel="noopener noreferrer">
+              <a class='htv' href="https://www.youtube.com/watch?v=xtbEpU1VjFE" target="_blank" rel="noopener noreferrer">
                 <h4>Youtube: Hack The Virus - CoCare</h4>
                   <img class="youtube" src={Youtube} /></a>
         </div> 
@@ -78,8 +79,21 @@ const App = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg>
           </a>
-          <p>Developed a full-stack web-application using surge and heroku to host it on the internet. The front-end portion was built using React.js with react-router to create different pages as well desig using css. Used Ruby on Rails on the back end to create a database to store data from enrollment form. Configured DNS to netlify to host with a secure connection on the internet. Applied google analytics and running online promotion campaign. </p>
-          <p class="highlights"> React.js</p>
+          <p>Developed a full-stack web-application using Netlify and Heroku to host it. The front-end portion was built using React.js with react-router to create different pages as well designing using css. Used Ruby on Rails on the back end to create a database to store data from the enrollment form. Configured DNS to netlify to host with a secure connection on the internet. Applied google analytics and running online promotion campaign through instagram. </p>
+          <p class="highlights"> Ruby on Rails, React.js, Heroku, Netlify, Google Analytics, PostgreSQL</p>
+            </div>
+            <div class="work-piece">
+              
+            <a target="_blank" rel="noopener noreferrer" class="work" href="http://dogsittingnyc.com/">
+            <img class="tennis-ball" src={Pups} />
+            Dog Sitting
+              </a>
+          <a target="_blank" rel="noopener noreferrer" class="css-13nadwe" class="work" href="https://github.com/yaroAmaro219/Dog-Sitting">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg>
+          </a>
+          <p>Developed a full-stack web-application using Netlify and Heroku to host it. The front-end portion was built using React.js with react-router to create different pages as well designing using css. Used Ruby on Rails on the back end to create a database to store data from the enrollment form. Configured DNS to netlify to host with a secure connection on the internet. Applied google analytics and running online promotion campaign through instagram. </p>
+          <p class="highlights"> Ruby on Rails, React.js, Heroku, Netlify, Google Analytics, PostgreSQL</p>
             </div>
             <div class="work-piece">
               <img class="octago" src={octago} />
