672f05b1e7223e0090e47ddc274e99c45ced2b2f
Artemie Amari <yaroamari@gmail.com>
2021-07-16 11:09:43 -0400

    commit

--- src/components/Insights.js
+++ src/components/Insights.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import image1 from '../images/pexels-abigail-olarte-6011998.jpg'
+import image2 from '../images/pexels-ethan-jones-3222401.jpg'
+import image3 from '../images/pexels-olya-prutskova-7121392.jpg'
+import image4 from '../images/pexels-photo-6039238.jpg'
 
 export default class Insights extends Component {
   render() {
     return (
       <div class='insights-container'>
         <div class='insights-page'>
-        <h1> Gallery</h1>
+          <br/>
+          <h1 class='about-title'>Gallery</h1>
+          <br/>
+          <div class='gallery'>
+            <img class='gallery-img' src={image1} />
+            <img class='gallery-img' src={image2} />
+          </div>
+          <div class='gallery'>
+            <img class='gallery-img' src={image3} />
+            <img class='gallery-img' src={image4} />
+          </div>
         </div>
       </div>
     )
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import {Button} from 'react-bootstrap'
 
 export default class Services extends Component {
   render() {
     return (
       <div class='services-page'>
-        <h1>Services</h1>
+        <h1 class='about-title'>Services</h1>
+        <div class='services'>
+          <div class='service'>
+            <h3>Single Photo</h3>
+            <h4>$249</h4>
+            <Button>Book Now</Button>
+          </div>
+          <br/>
+          <div class='service'>
+            <h3>Two Photos</h3>
+            <h4>$449</h4>
+            <Button>Book Now</Button>
+          </div>
+          <br/>
+          <div class='service'>
+            <h3>Three Photos</h3>
+            <h4>$599</h4>
+            <h4>20% discount Best Value</h4>
+            <Button>Book Now</Button>
+          </div>
+        </div>
       </div>
     )
   }
